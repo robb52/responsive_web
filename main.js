0e5548e688cf221294e0b2794ce8bc72c203359b
Rob Brown <brown4homes@yahoo.com>
2020-04-08 12:02:46 -0700

    Added sticky function to nav

--- main.js
+++ main.js
@@ -8,4 +8,14 @@ $(document).ready(function(){
         $(".full-nav").removeClass("open");
     });
 
+    $(window).scroll(function(){
+        var sc = $(window).scrollTop();
+            if(sc > 100){
+                $(".nav").addClass("sticky");
+            }
+            else{
+                $(".nav").removeClass("sticky");
+            }
+    });
+
 });
