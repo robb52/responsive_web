c39d8a31a6ba8ce2a4e40d63c7c9e94c9f660f87
Rob Brown <brown4homes@yahoo.com>
2020-04-09 16:41:02 -0700

    Added markup and styled about us page

--- main.js
+++ main.js
@@ -18,4 +18,14 @@ $(document).ready(function(){
             }
     });
 
+    $('.bxslider').bxSlider({
+        mode: 'horizontal',
+        moveSlides: 1,
+        slideMargin: 40,
+        infiniteLoop: true,
+        minSlides: 1,
+        maxSlides: 1,
+        speed: 1200,
+    });
+
 });
